0d20787564e1cbda29a89c9d2fd03744ebf058d3
Totosuki <e231kari@gmail.com>
2025-11-13 15:38:12 +0900

    ステップ1が完成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprint!("引数の個数が正しくありません\n");
+        process::exit(1);
+    }
+
+    print!(".intel_syntax noprefix\n");
+    print!(".globl main\n");
+    print!("main:\n");
+    print!("  mov rax, {}\n", args[1]);
+    print!("  ret\n");
 }
